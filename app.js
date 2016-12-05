c6a2ca5c62aab0c434da8ee08e47be83387c9257
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2016-12-05 22:50:46 +1100

    011

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var about = require('./routes/about');
+var services = require('./routes/services');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/about', about);
+app.use('/services', services);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
